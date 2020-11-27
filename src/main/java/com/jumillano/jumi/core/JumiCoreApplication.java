5debee2abebf4d44f7ef3d67758ef89c2e40c2fd
jrojas <jonathanrojas31@gmail.com>
2020-11-27 00:30:02 -0300

    add project skeleton

---
+++ src/main/java/com/jumillano/jumi/core/JumiCoreApplication.java
@@ -0,0 +1,13 @@
+package com.jumillano.jumi.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumiCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumiCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumillano/jumi/core/JumiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumillano.jumi.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumiCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
